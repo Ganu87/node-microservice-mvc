4cf9a9626e89a01048982e826a6ca5fc45399783
ganesh shelke <shelke.ganesh87@gmail.com>
2024-12-30 15:54:16 +0530

    Implement the jest test cases

--- order-service/index.js
+++ order-service/index.js
@@ -4,8 +4,12 @@ const orderRoutes = require("./routes/orderRoutes.js");
 const app = express();
 app.use(express.json());
 console.log("in order index.js");
-app.use('/orders',orderRoutes)
+app.use('/orders', orderRoutes)
 
 
 // Start server
-app.listen(3002, () => console.log('Order Service running on port 3002'));
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(3002, () => console.log('Order Service running on port 3002'));
+}
+
+module.exports = app;
